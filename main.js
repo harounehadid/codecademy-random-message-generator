428e64442079f33296eb47b982a044808ea7aa46
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2022-01-30 14:19:20 +0100

    Store message components in different arrays

---
+++ main.js
@@ -0,0 +1,19 @@
+// Storing the quotes' parts in different arrays
+const mesStart = ['Spread love everywhere you go ', 'When you reach the end of your rope ', 
+'Always remember ', "Don't judge each day by ", 'The future belongs to ', 
+'It is during our darkest moments that ', 'Do not go where the path may lead ', 
+'Life is a succession of lessons ', 'You will face many defeats in life ', 
+'Life is never fair ', 'You only live once ', 'Live in the sunshine ', 
+'Go confidently in the direction of your dreams ', 'Life is really simple ', 
+'Life itself is the '];
+const mesMiddle = ['let no one ever come to you ', 'tie a knot in it ', 
+'that you are absolutely unique ', 'the harvest you reap but ', 'those who believe in ', 
+'we must focus to ', 'go instead where there is no path ', 'which must be lived to ', 
+'but never let yourself ', 'and perhaps it is a good thing for most of us ', 
+'but if you do it right ', 'swim the sea ', 'live the life ', 'but we insist on making it ', 
+'most wonderful '];
+const mesEnd = ['without leaving happier.', 'and hang on.', 'just like everyone else.', 
+'by the seeds that you plant.', 'the beauty of their dreams.', 'see the light.', 
+'and leave a trail.', 'be understood.', 'be defeated.', 'that it is not.', 'once is enough.', 
+'drink the wild air.', "you've imagined.", 'complicated.', 'fairy tale.'];
+
